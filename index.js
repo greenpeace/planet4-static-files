06ccc98a3e272a69713ef3ecff0babfea3de28c3
Nikos Roussos <nikos@roussos.cc>
2021-03-01 12:55:04 +0200

    Increase cache TTL to 1 year
    This is a recommendation from lighthouse, since our static files are immutable.

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ addEventListener('fetch', event => {
 const getStorageUrl = cdnUrl => {
   const url = new URL(cdnUrl);
   const path = url.pathname;
-  
+
   // remove the /static path to match the bucket name
-  const resource = path.substring(7);  
+  const resource = path.substring(7);
   const target = 'storage.googleapis.com';
-  
+
   url.hostname = target;
   url.pathname = resource;
 
@@ -27,8 +27,8 @@ async function handle(request) {
   let response = await fetch(originRequest, {
     cf: {
       // Always cache this fetch regardless of content type
-      // for a max of 31 days before revalidating the resource
-      cacheTtl: 2678400,
+      // for a max of 1 year before revalidating the resource
+      cacheTtl: 31536000,
       cacheEverything: true,
     },
   });
